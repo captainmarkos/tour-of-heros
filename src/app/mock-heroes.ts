686010cbb0a7eb8ca8ad7bf4bcd062017244d30b
Mark Brettin <mark@jackrabbitsystems.com>
2017-12-27 11:15:59 -0700

    Displays a list of heroes.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jessica Jones' },
+  { id: 12, name: 'Captain America' },
+  { id: 13, name: 'Yoda' },
+  { id: 14, name: 'Darth Plagueis' },
+  { id: 15, name: 'Obi-Wan Kenobi' },
+  { id: 16, name: 'Wonder Woman' },
+  { id: 17, name: 'Superman' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Qui-Gon Jinn' },
+  { id: 20, name: 'Thor' }
+];
