8791a3b8f52b358557a1d2bb3c41a35a4f6b4c67
Mark Brettin <mark@jackrabbitsystems.com>
2017-12-29 03:56:11 -0700

    moves images into assets/images/

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Jessica Jones', img_url: '/assets/Jessica-Jones.jpg' },
-  { id: 12, name: 'Captain America', img_url: '/assets/Captain-America.jpg' },
-  { id: 13, name: 'Yoda', img_url: '/assets/Yoda-Revenge-of-the-Sith.jpg' },
-  { id: 14, name: 'Darth Plagueis', img_url: '/assets/Darth_Plagueis_2.jpg' },
-  { id: 15, name: 'Obi-Wan Kenobi', img_url: '/assets/obi-wan.jpg' },
-  { id: 16, name: 'Wonder Woman', img_url: '/assets/wonder-woman-10.jpg' },
-  { id: 17, name: 'Superman', img_url: '/assets/super-man.jpg' },
-  { id: 18, name: 'Hulk', img_url: '/assets/ThorRagnarok.jpg' },
-  { id: 19, name: 'Qui-Gon Jinn', img_url: '/assets/' },
-  { id: 20, name: 'Thor', img_url: '/assets/' }
+  { id: 11, name: 'Jessica Jones', img_url: '/assets/images/Jessica-Jones.jpg' },
+  { id: 12, name: 'Captain America', img_url: '/assets/images/Captain-America.jpg' },
+  { id: 13, name: 'Yoda', img_url: '/assets/images/Yoda-Revenge-of-the-Sith.jpg' },
+  { id: 14, name: 'Darth Plagueis', img_url: '/assets/images/Darth_Plagueis_2.jpg' },
+  { id: 15, name: 'Obi-Wan Kenobi', img_url: '/assets/images/obi-wan.jpg' },
+  { id: 16, name: 'Wonder Woman', img_url: '/assets/images/wonder-woman-10.jpg' },
+  { id: 17, name: 'Superman', img_url: '/assets/images/super-man.jpg' },
+  { id: 18, name: 'Hulk', img_url: '/assets/images/ThorRagnarok.jpg' },
+  { id: 19, name: 'Qui-Gon Jinn', img_url: '/assets/images/Qui-Gon_Jinn_400x300.jpg' },
+  { id: 20, name: 'Thor', img_url: '/assets/images/Thor-Ragnarok-Reviews-Big.jpg' }
 ];
