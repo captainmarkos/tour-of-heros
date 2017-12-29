cc1b49d65e5fd27c1d54252b2519deb307a793d7
Mark Brettin <mark@jackrabbitsystems.com>
2017-12-28 19:00:40 -0700

    adds img_url to Hero and HeroDetail component

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,5 @@
 export class Hero {
   id: number;
   name: string;
+  img_url: string;
 }
--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Jessica Jones' },
-  { id: 12, name: 'Captain America' },
-  { id: 13, name: 'Yoda' },
-  { id: 14, name: 'Darth Plagueis' },
-  { id: 15, name: 'Obi-Wan Kenobi' },
-  { id: 16, name: 'Wonder Woman' },
-  { id: 17, name: 'Superman' },
-  { id: 18, name: 'Hulk' },
-  { id: 19, name: 'Qui-Gon Jinn' },
-  { id: 20, name: 'Thor' }
+  { id: 11, name: 'Jessica Jones', img_url: '/assets/Jessica-Jones.jpg' },
+  { id: 12, name: 'Captain America', img_url: '/assets/Captain-America.jpg' },
+  { id: 13, name: 'Yoda', img_url: '/assets/Yoda-Revenge-of-the-Sith.jpg' },
+  { id: 14, name: 'Darth Plagueis', img_url: '/assets/Darth_Plagueis_2.jpg' },
+  { id: 15, name: 'Obi-Wan Kenobi', img_url: '/assets/obi-wan.jpg' },
+  { id: 16, name: 'Wonder Woman', img_url: '/assets/wonder-woman-10.jpg' },
+  { id: 17, name: 'Superman', img_url: '/assets/super-man.jpg' },
+  { id: 18, name: 'Hulk', img_url: '/assets/ThorRagnarok.jpg' },
+  { id: 19, name: 'Qui-Gon Jinn', img_url: '/assets/' },
+  { id: 20, name: 'Thor', img_url: '/assets/' }
 ];
